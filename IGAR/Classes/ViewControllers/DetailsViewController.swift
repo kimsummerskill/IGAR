178171afeb0e0084248ee02c6c3074dc542c4ea7
Lewis Brown <Lewis.Brown@ig.com>
2018-10-09 12:44:45 +0100

    basic contraints and iboutlet added for webview

--- IGAR/Classes/ViewControllers/DetailsViewController.swift
+++ IGAR/Classes/ViewControllers/DetailsViewController.swift
@@ -14,6 +14,7 @@ class DetailsViewController: UIViewController, MVVMViewControllerProtocol {
     @IBOutlet var highLabel: UILabel!
     @IBOutlet var titleLabel: UILabel?
     @IBOutlet var image: UIImageView?
+    @IBOutlet var webView: UIWebView?
     var viewModel: DetailsViewModel!
     
     var tickerName: String = ""
